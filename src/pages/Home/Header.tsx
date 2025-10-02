2a88b14099b293c5fe1d8c364403162964034d94
Jeferson Lugo <jefersoneduardoluego@gmail.com>
2025-10-02 15:08:48 -0400

    some changes to build

--- src/pages/Home/Header.tsx
+++ src/pages/Home/Header.tsx
@@ -1,14 +1,8 @@
 import { CiSearch } from 'react-icons/ci'
-import { MdOutlineLocalOffer, MdOutlineWebStories } from 'react-icons/md'
+// import { MdOutlineLocalOffer, MdOutlineWebStories } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 
-export default function Header({
-	isStories,
-	toggleStories,
-}: {
-	isStories: boolean
-	toggleStories: () => void
-}) {
+export default function Header() {
 	return (
 		<div className='flex items-center justify-between gap-2 px-2 py-1'>
 			<h1 className='text-2xl font-semibold'>Stor</h1>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
 import Footer from './Footer'
 import Header from './Header'
 import Post from './Post'
 // import StoriesAndOffers from './StoriesAndOffers'
 export default function Home() {
-	const [isStories, setIsStories] = useState(true)
+	// const [isStories, setIsStories] = useState(true)
 
-	const toggleStories = () => setIsStories(!isStories)
+	// const toggleStories = () => setIsStories(!isStories)
 	return (
 		<div>
-			<Header isStories={isStories} toggleStories={toggleStories} />
+			<Header />
 			{/* <StoriesAndOffers isStories={isStories} /> */}
 			<section className='mt-4'>
 				<Post />
